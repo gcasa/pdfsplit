f1f336f146834a051bb6530032894d738dd15fff
Gregory Casamento <greg.casamento@gmail.com>
2020-03-08 16:07:27 -0400

    Add PyPDF2

--- pdfsplit.py
+++ pdfsplit.py
@@ -3,6 +3,7 @@ import os
 
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
+
 def pdf_splitter(path):
     fname = os.path.splitext(os.path.basename(path))[0]
     pdf = PdfFileReader(path)
@@ -10,10 +11,11 @@ def pdf_splitter(path):
         pdf_writer = PdfFileWriter()
         pdf_writer.addPage(pdf.getPage(page))
         output_filename = '{}_page_{}.pdf'.format(
-            fname, page+1)
+            fname, page + 1)
         with open(output_filename, 'wb') as out:
             pdf_writer.write(out)
         print('Created: {}'.format(output_filename))
+
 
 if __name__ == '__main__':
     path = 'w9.pdf'

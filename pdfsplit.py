fcf536a7248222663c1a75a725bd8ffb1cddf5ff
Gregory Casamento <greg.casamento@gmail.com>
2020-03-08 16:35:41 -0400

    Completed change to pull 5 pages per pdf file

--- pdfsplit.py
+++ pdfsplit.py
@@ -7,17 +7,17 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 def pdf_splitter(path, n):
     fname = os.path.splitext(os.path.basename(path))[0]
     pdf = PdfFileReader(path)
-    for page in range(pdf.getNumPages()):
-        pdf_writer = PdfFileWriter()
-        pdf_writer.addPage(pdf.getPage(page))
+    pdf_writer = PdfFileWriter()
 
+    for page in range(pdf.getNumPages()):
+        pdf_writer.addPage(pdf.getPage(page))
         output_filename = '{}_page_{}.pdf'.format(
             fname, page + 1)
-
         if page % n == 0:
             with open(output_filename, 'wb') as out:
                 pdf_writer.write(out)
             print('Created: {}'.format(output_filename))
+            pdf_writer = PdfFileWriter()
 
 
 def main():

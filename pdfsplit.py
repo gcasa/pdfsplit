7ecfde82185858c611639deb1fa91e67f6f6d5a2
Gregory Casamento <greg.casamento@gmail.com>
2020-03-08 19:24:04 -0400

    Add new dirs

--- pdfsplit.py
+++ pdfsplit.py
@@ -12,9 +12,16 @@ def pdf_splitter(path, n):
 
     for page in range(pdf.getNumPages()):
         pdf_writer.addPage(pdf.getPage(page))
+
+        # Figure out the start page....
+        p = 1
+        if page - 4 > 0:
+            p = page - 4
+
+        # Output the file
         output_filename = '{}_page_{}-{}.pdf'.format(
-            fname, page - 4, page)
-        if (page % n == 0 and page != 0) or page == num_pages:
+            fname, p, page)
+        if (page % n == 0 and page != 0) or page == (num_pages - 1):
             with open(output_filename, 'wb') as out:
                 pdf_writer.write(out)
             print('Created: {}'.format(output_filename))
@@ -22,10 +29,13 @@ def pdf_splitter(path, n):
 
 
 def main():
+    i = 0
     directory = r'/Users/heron/PDFS'
     for entry in os.scandir(directory):
         if entry.path.endswith(".pdf"):
+            i = i + 1
             pdf_splitter(entry.path, 5)
+    print('Processed {} files'.format(i))
 
 
 if __name__ == '__main__':

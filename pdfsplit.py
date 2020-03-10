8f5ceddc3aeea3bdc9b882f96beb5351a4866a8a
Gregory Casamento <greg.casamento@gmail.com>
2020-03-10 02:38:46 -0400

    Add code to count all of the PDFs which will be produced.

--- pdfsplit.py
+++ pdfsplit.py
@@ -9,6 +9,7 @@ def pdf_splitter(path, n):
     pdf = PdfFileReader(path)
     pdf_writer = PdfFileWriter()
     num_pages = pdf.getNumPages()
+    num_files = 0
 
     for page in range(pdf.getNumPages()):
         pdf_writer.addPage(pdf.getPage(page))
@@ -21,16 +22,21 @@ def pdf_splitter(path, n):
                 pdf_writer.write(out)
             print('Created: {}'.format(output_filename))
             pdf_writer = PdfFileWriter()
+            num_files = num_files + 1
+
+    return num_files
 
 
 def main():
     i = 0
+    files = 0
     directory = r'/Users/heron/PDFS'
     for entry in os.scandir(directory):
         if entry.path.endswith(".pdf"):
             i = i + 1
-            pdf_splitter(entry.path, 5)
+            files += pdf_splitter(entry.path, 5)
     print('Processed {} files'.format(i))
+    print('Created {} files'.format(files))
 
 
 if __name__ == '__main__':
